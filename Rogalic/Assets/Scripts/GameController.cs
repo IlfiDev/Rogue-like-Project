f9756e71a9f8e434703dbf299edb4f56e221b417
EugeneKzxc <minengisob@gmail.com>
2022-07-30 12:15:59 +0300

    30.07.22

--- Rogalic/Assets/Scripts/GameController.cs
+++ Rogalic/Assets/Scripts/GameController.cs
@@ -1,35 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject platform;
+    public GameObject StartRoom;
+    public GameObject Room;
     public GameObject enemy;
-    public GameObject wayPoint;
-    public GameObject PointsFolder;
     public GameObject player;
-    public GameObject playerSpawnPoint;
+    private GameObject playerSpawnPoint;
     public Vector3 spawnValues;
     public GameObject main_camera;
 
-    float platformSizeX;
-    float platformSizeZ;    
+    float startRoomSizeX;
+    float startRoomSizeZ;
+    float startRoomPositionX;
+    float startRoomPositionZ;
 
     void Start()
     {
-        platformSizeX = platform.transform.localScale.x;
-        platformSizeZ = platform.transform.localScale.z;
+        startRoomSizeX = StartRoom.transform.localScale.x;
+        startRoomSizeZ = StartRoom.transform.localScale.z;
+
+        startRoomPositionX = StartRoom.transform.position.x;
+        startRoomPositionZ = StartRoom.transform.position.z;
+
+        CreateStartRoom();
+        GenerateRooms();
+
+        playerSpawnPoint = GameObject.FindGameObjectWithTag("PlayerSpawnPoint");
 
         SpawnPlayer();
         SpawnCamera();
-        SpawnWayPoints();
         SpawnCreature();
+    }
+
+    void CreateStartRoom()
+    {
+        Vector3 spawnPosition = new Vector3(0f, 0f, 0f);
+        Quaternion spawnRotation = Quaternion.identity;
+
+        Instantiate(StartRoom, spawnPosition, spawnRotation);
     }
 
     void SpawnCreature()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-platformSizeX / 2 + 5, platformSizeX / 2 - 5), 1f, Random.Range(-platformSizeZ / 2 + 5, platformSizeZ / 2 - 5));
+        Vector3 spawnPosition = new Vector3(Random.Range(-startRoomSizeX / 2 + 5, startRoomSizeX / 2 - 5), 1f, Random.Range(-startRoomSizeZ / 2 + 5, startRoomSizeZ / 2 - 5));
         Quaternion spawnRotation = Quaternion.identity;
 
         Instantiate(enemy, spawnPosition, spawnRotation);  
@@ -51,14 +68,26 @@ public class GameController : MonoBehaviour
         Instantiate(main_camera, spawnPosition, spawnRotation);
     }
 
-    void SpawnWayPoints()
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            Vector3 spawnPosition = new Vector3(Random.Range(-platformSizeX / 2 + 5, platformSizeX / 2 - 5), 1f, Random.Range(-platformSizeZ / 2 + 5, platformSizeZ / 2- 5));
-            Quaternion spawnRotation = Quaternion.identity;
+    //void SpawnWayPoints()
+    //{
+    //    for (int i = 0; i < 100; i++)
+    //    {
+    //        Vector3 spawnPosition = new Vector3(Random.Range(-platformSizeX / 2 + 5, platformSizeX / 2 - 5), 1f, Random.Range(-platformSizeZ / 2 + 5, platformSizeZ / 2- 5));
+    //        Quaternion spawnRotation = Quaternion.identity;
 
-            Instantiate(wayPoint, spawnPosition, spawnRotation, PointsFolder.transform);
-        }
+    //        Instantiate(wayPoint, spawnPosition, spawnRotation, PointsFolder.transform);
+    //    }
+    //}
+
+    void GenerateRooms()
+    {
+        int[,] map = {{ },{ }};
+
+
+
+        Vector3 spawnPosition = new Vector3(startRoomPositionX + startRoomSizeX, 0f, 0f);
+        Quaternion spawnRotation = Quaternion.identity;
+
+        Instantiate(Room, spawnPosition, spawnRotation);        
     }
 }
