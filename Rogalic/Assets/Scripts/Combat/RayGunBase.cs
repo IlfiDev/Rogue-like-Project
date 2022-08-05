6d0630235316776b9b74afac9483cc280b6af952
IlfiDev <ilyafillipov2@gamil.com>
2022-08-05 16:57:59 +0300

    Deleted some Scripts and prefabs

--- Rogalic/Assets/Scripts/Combat/RayGunBase.cs
+++ Rogalic/Assets/Scripts/Combat/RayGunBase.cs
@@ -2,26 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayGunBase : MonoBehaviour
+public class RayGunBase : Weapon, IAttack 
 {
-  [SerializeField] protected float rayWidth = 0.1f;
-  [SerializeField] protected GameObject ray;
-  [SerializeField] protected float scatter = 1f;
-  [SerializeField] protected Transform shootingPoint;
-  [SerializeField] protected int numberOfShots = 1;
-  [SerializeField] protected float knockback;
-  [SerializeField] protected float startupTime = 0;
-  protected bool canAttack = true;
-  protected List<GameObject> rays = new List<GameObject>();
-  protected float timeStamp;
-  [SerializeField] protected float cooldownTime = 1f; 
-
-  protected void checkCooldown(){
-		if(timeStamp <= Time.time){
-			canAttack = true;
-		}
-		else{
-			canAttack = false;
-		}
-   }
+	
 }
