d5ef4e1e181e90b4807d778661d746b9470fbcda
IlfiDev <ilyafillipov2@gmail.com>
2022-10-12 14:06:09 +0300

    feature: add ray and raygun

--- Rogalic/Assets/Scripts/Combat/RayGun.cs
+++ Rogalic/Assets/Scripts/Combat/RayGun.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class RayGun : Gun, IAttack
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField]
+    private LayerMask _layerMask;
+    [SerializeField]
+    private GameObject _rayObject;
+    private GameObject _rayInstance;
     void Update()
     {
 		checkCooldown(); 
+    }
+
+    public void Attack(float damageMultiplier, Vector3 target){
+        Ray ray = new Ray(shootingPoint.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 100f, _layerMask, QueryTriggerInteraction.Ignore)){
+            if(_rayInstance == null){
+
+                _rayInstance = Instantiate(_rayObject);
+            }
+
+            
+            _rayInstance.GetComponent<RayBehaviour1>().UpdateRay(Vector3.Distance(shootingPoint.position, hit.point), (hit.point + shootingPoint.position) / 2, shootingPoint);
+        }
     }
 }
