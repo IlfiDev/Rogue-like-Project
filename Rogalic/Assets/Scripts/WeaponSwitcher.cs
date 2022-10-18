a203a1e6ebc658f89ec70051c4609c06f063d059
IlfiDev <ilyafillipov2@gmail.com>
2022-10-18 22:02:40 +0300

    fix: fix interactable (1)

--- Rogalic/Assets/Scripts/WeaponSwitcher.cs
+++ Rogalic/Assets/Scripts/WeaponSwitcher.cs
@@ -71,7 +71,7 @@ public class WeaponSwitcher : MonoBehaviour
                         _weaponPoint.rotation);
             _weapons.Add(obj);
             _weapons[i].transform.parent = _weaponPoint.parent;
-            if(_weapons[i].transform.name != _defaultWeapon.transform.name){
+            if(_weapons[i].transform.tag != _defaultWeapon.transform.tag){
                 _weapons[i].GetComponent<Interactible>().enabled = false;
             }
             _weapons[i].SetActive(false);
